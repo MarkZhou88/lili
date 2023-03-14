35d66893c4503bc37684b6732b7711ccb431da3c
xiaozhouzhoua <1445101933@qq.com>
2023-03-14 12:37:06 +0800

    Initial commit

---
+++ src/main/java/com/lili/LiliApplication.java
@@ -0,0 +1,13 @@
+package com.lili;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lili/LiliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lili;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
