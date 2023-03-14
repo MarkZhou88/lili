66daab4e08cf3337c281bc49b24965021bd4ec8a
xiaozhouzhoua <1445101933@qq.com>
2023-03-14 12:46:04 +0800

    first api

---
+++ src/main/java/com/lili/api/TimeApi.java
@@ -0,0 +1,14 @@
+package com.lili.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@RestController
+public class TimeApi {
+    @GetMapping
+    public String time() {
+        return DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(LocalDateTime.now());
+    }
+}
